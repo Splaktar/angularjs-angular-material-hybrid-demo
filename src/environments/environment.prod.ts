4a5cebbede005dd4b52e84c79b0dfb2c4b2c0589
Michael Prentice <splaktar@gmail.com>
2021-05-28 06:44:55 -0400

    build: update to Angular, CLI, and Material to v12
    - update other dependencies
    - fix npm audit warnings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
-import { version } from '../../package.json';
+import packageInfo from '../../package.json';
 
 export const environment = {
   production: true,
-  version,
+  version: packageInfo.version,
 };
